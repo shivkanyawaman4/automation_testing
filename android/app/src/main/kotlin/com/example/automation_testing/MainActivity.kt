1aae79eaa4386097b34f85864ad4a9212d56318d
shivkanyawaman4 <wamanshivkanya@gmail.com>
2022-10-15 10:41:50 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/automation_testing/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.automation_testing
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ test_driver/test.dart
@@ -0,0 +1,7 @@
+import 'package:automation_testing/main.dart' as app;
+import 'package:flutter_driver/driver_extension.dart';
+
+void main() {
+   enableFlutterDriverExtension();
+  app.main();
+}
